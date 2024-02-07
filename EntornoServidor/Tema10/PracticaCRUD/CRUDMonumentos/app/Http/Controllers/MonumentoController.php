79f07b7f2b345e5fe1452be52221e53629db831d
marrdo <manuelmaldonado930329@gmail.com>
2024-02-07 12:55:04 +0100

    Parte del CRUD

---
+++ EntornoServidor/Tema10/PracticaCRUD/CRUDMonumentos/app/Http/Controllers/MonumentoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMonumentoRequest;
+use App\Http\Requests\UpdateMonumentoRequest;
+use App\Models\Monumento;
+
+class MonumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMonumentoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Monumento $monumento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Monumento $monumento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMonumentoRequest $request, Monumento $monumento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Monumento $monumento)
+    {
+        //
+    }
+}
---
+++ EntornoServidor/Tema10/PracticaCRUD/CRUDMonumentos/app/Models/Monumento.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Monumento extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
